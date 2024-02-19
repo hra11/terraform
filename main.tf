194fc924cee17b9e052dce7be32cf132bbb6a5dd
hra11 <160456928+hra11@users.noreply.github.com>
2024-02-19 12:17:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+# terraform block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.3.0"
+    }
+  }
+}
+
+# provider block
+provider "aws" {
+  region = "ap-southeast-1" #Asia Pacific (Singapore)#
+}
+
+
 # Data source:  Get AMI ID for Amazon Linux2 OS
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
